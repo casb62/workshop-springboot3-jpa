03b1a9047552f63ebce6d647596f083184d92dbe
casb62 <casb62@gmail.com>
2024-06-24 09:22:43 -0300

    Exception handling - delete

--- src/main/java/com/casb/curso/services/UserService.java
+++ src/main/java/com/casb/curso/services/UserService.java
@@ -2,10 +2,15 @@ package com.casb.curso.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
 import com.casb.curso.entities.User;
 import com.casb.curso.repositories.UserRepository;
+import com.casb.curso.services.exceptions.DatabaseException;
 import com.casb.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
